f2eb27d5aa8ab487218b55e1a4f5a8c60925d828
aorazalin <dorazalin.alibek@gmail.com>
2024-05-16 14:56:21 +0800

    Initial project frame

---
+++ rehlinepo/optimization/_base.py
@@ -0,0 +1,6 @@
+class Portfolio:
+    def __init__(self):
+        pass
+
+    def gen_portf(self, prices):
+        pass
---
+++ rehlinepo/optimization/_mean_variance.py
@@ -0,0 +1,8 @@
+from ._base import Portfolio
+
+class MeanVariance(Portfolio):
+    def __init__(self):
+        pass
+
+    def get_portf(self, prices):
+        pass
